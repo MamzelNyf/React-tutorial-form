7ed23cc3762c307a1a4e6575e2e9598d89baf1c3
MamzelNyf <you@example.comfanny.lefert@gmail.com>
2019-09-19 20:53:47 +0800

    push build folder

---
+++ build/precache-manifest.454525373ba8856e78aff153b2ac9214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e618c05c862d1faad8c6e5f0b3c9176c",
+    "url": "/MamzelNyf/react-tutorial-form/index.html"
+  },
+  {
+    "revision": "e4544b304be9e5bf608d",
+    "url": "/MamzelNyf/react-tutorial-form/static/css/main.d8f58129.chunk.css"
+  },
+  {
+    "revision": "bced947ef4ae0ed66063",
+    "url": "/MamzelNyf/react-tutorial-form/static/js/2.ebb8b90c.chunk.js"
+  },
+  {
+    "revision": "e4544b304be9e5bf608d",
+    "url": "/MamzelNyf/react-tutorial-form/static/js/main.13009854.chunk.js"
+  },
+  {
+    "revision": "a00a0cae92826dc09fa5",
+    "url": "/MamzelNyf/react-tutorial-form/static/js/runtime~main.58d91517.js"
+  }
+]);
